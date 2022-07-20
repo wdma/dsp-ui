b7c4af95dc5191f6d6607ded56ed96aa2b1aa2bb
wdma <wdma@users.noreply.github.com>
2022-07-20 12:08:12 -0400

    adding data for catalog ui

---
+++ metadata/catalog.mjs
@@ -0,0 +1,242 @@
+export const catalog = {
+    "ES60-RAW-DBT_20171009-215644": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171009-215644.Raw",
+      "filetype": "csv",
+      "ascession": "20200108",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171009-215644": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171009-215644.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171010-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171010-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171011-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171011-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171012-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171012-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171013-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171013-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171014-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171014-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171015-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171015-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171016-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171016-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171017-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171017-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171018-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171018-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171019-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171019-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171020-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171020-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171021-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171021-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171022-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171022-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171023-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171023-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171024-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171024-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171025-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171025-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171026-000000": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171026-000000.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171026-175215": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171026-175215.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171026-182719": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171026-182719.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  },
+    "ES-SDDBT-Bridge-Message_20171026-183311": {
+      "url": "https://www.nodc.noaa.gov/archive/arc0113/0167795/1.1/data/0-data/SH-0_2017-10-31-154706/ES60%20-%20Bridge%20%20Depth%20Sounder/ES-SDDBT-Bridge-Message_20171026-183311.Raw",
+      "filetype": "csv",
+      "ascession": "20220628",
+      "templates": {
+        "SCS DBT": {
+          "location": "../templates/SCSDBT.mjs"
+        }
+      }
+  }
+};
+
+
+export const options = function(source) {
+
+  let headers = new Headers({
+    'Content-Type': 'text/csv',
+    'responseType': 'blob'
+  });
+
+  let init = {
+    method: 'GET',
+    headers: headers,
+    mode: 'cors',
+    cache: 'default'
+  };
+ 
+  //let options = new Request(source, init);
+
+  return init
+}
